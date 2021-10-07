add805064a4644195f6ac27056b12777ff86e0db
OneMadness <magamag268@gmail.com>
2021-10-07 17:02:11 +0200

    Added telegram bot api and youtube-dl

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 const telegramApi = require("node-telegram-bot-api");
-const token = "yourAPI";
+const token = "2064361463:AAHQYZdz6dFSXDKjy_ETU0L43GQFh-hZXv4";
+const fs = require("fs");
+const youtubedl = require("youtube-dl-exec");
 
 const bot = new telegramApi(token, { polling: true });
 
-bot.on("message", (msg) => {
-  console.log(msg);
-});
+// let video = "";
+
+const start = () => {
+  bot.on("message", async (msg) => {
+    const text = msg.text;
+    const chatId = msg.chat.id;
+    const username = msg.chat.first_name;
+    const mode = "youtube";
+
+    if (text === "/start") {
+      await bot.sendSticker(
+        chatId,
+        "https://tlgrm.ru/_/stickers/b50/063/b5006369-8faa-44d7-9f02-1ca97d82cd49/1.webp"
+      );
+      await bot.sendMessage(chatId, "Пошла жара...");
+    }
+    if (mode === "youtube") {
+      console.log("Test video sending");
+      const video = await youtubedl(text, {
+        noWarnings: true,
+        preferFreeFormats: true,
+      }).then((output) => console.log(output));
+      bot.sendMessage(chatId, output);
+    }
+  });
+};
+
+start();
